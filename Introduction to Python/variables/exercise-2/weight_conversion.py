f0f129c7e156039437c701b264297863cd415408
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 16:01:30 +0200

    weight-conversion

---
+++ Introduction to Python/variables/exercise-2/weight_conversion.py
@@ -0,0 +1,10 @@
+# let's convert weight in kilograms to pounds
+
+# 1 kilogram is 2.20462 pounds
+
+weight = float(input(" Enter your weight in kilograms: "))
+
+conversion = weight * 2.20462
+
+print("Your weight in pounds is: "+str(conversion))
+
