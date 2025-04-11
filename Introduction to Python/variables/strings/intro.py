31de82eec6ba1e71884e0b589cde56a5357ee828
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 16:14:31 +0200

    intro-to-strings

---
+++ Introduction to Python/variables/strings/intro.py
@@ -0,0 +1,36 @@
+# We have single, double, and triple quotes which we can use in strings
+
+# single quote
+name = 'Patrick'
+print(name)
+
+#double quotes
+country = "Zimbabwe"
+print(country)
+
+#triple quotes
+message = '''
+
+Dear Patrick,
+
+Thank you for taking the time to apply for the Junior Python Developer 
+position. We appreciate your interest in joining our team and the 
+effort you put into your application.
+
+After careful consideration of your qualifications and experience, 
+we regret to inform you that you have not been selected for the 
+position at this time. This decision was not an easy one, as we 
+received applications from many talented individuals.
+
+We encourage you to continue pursuing opportunities in software 
+development, and we wish you the very best in your job search and 
+future endeavors.
+
+Thank you once again for your interest.
+
+Kind regards,
+
+
+'''
+
+print(message)
