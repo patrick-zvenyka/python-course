1b7d31799f54d48968c861da59df93fe084268c0
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 15:19:59 +0200

    intro-to-variables

---
+++ Introduction to Python/variables/intro.py
@@ -0,0 +1,45 @@
+# variables are used to hold different data types as shown below
+
+# 1. Numeric Types
+#int: integer
+age = 12
+
+#float: floating-point
+price = 56.99
+
+#complex: complex numbers
+z = 3 + 5j
+
+# 2. Text Type
+#str: string
+name = "Patrick"
+
+# 3. Boolean Type
+#bool: True or False
+is_logged_in = True
+
+# 4. Sequence Types
+#list: ordered, changeable, allows duplicates
+currency = ["ZAR","USD","ZWD"]
+
+#tuple: ordered, unchangeable, allows duplicates
+dimensions = (20, 40, 60)
+
+#range: used in loops
+numbers = range(9)
+
+# 5. Mapping Type
+#dict: key-value pairs
+student = {"name":"Patrick","age": 25}
+
+# 6. Set Types
+#set: unordered, no duplicates
+number_line = {1,2,3,4,5,6,7,8,9}
+
+#frozenset: like a set but immutable
+frozen = frozenset([1,2,3])
+
+# 7. Binary Types
+#bytes, bytearray, memoryview
+
+data = b"hello"
