5e8d2e371963f7b1106d602c9272aaa8e16b4621
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 16:23:33 +0200

    string-indexing

---
+++ Introduction to Python/variables/strings/string-indexing.py
@@ -0,0 +1,11 @@
+# strings are indexed starting from 0 and the last index is -1
+
+# 1. Basic positive indexing
+name = 'John'
+print(name[0])  # Output: J
+print(name[1])  # Output: o
+
+# 2. Negative indexing
+student = 'Sharon'
+print(student[-1])  # Output: n
+print(student[-2])  # Output: o
