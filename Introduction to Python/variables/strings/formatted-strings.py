346203434e6e9c032c662812b0b473e9eb3dce5b
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 16:42:15 +0200

    formatted-strings

---
+++ Introduction to Python/variables/strings/formatted-strings.py
@@ -0,0 +1,13 @@
+first = 'John'
+last = 'Doe'
+
+message = first +' '+last +' '+'is a coder'
+
+print(message)
+
+# using the ridiculous example above, here is how a formatted string looks like
+
+first = 'John'
+last = 'Doe'
+msg = f'{first} {last} is a coder'
+print(message)
