edb19a89a900ec08b3b900d4305c0fffc4d9d20c
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 14:57:51 +0200

    introduciton-message

---
+++ Introduction to Python/intro.py
@@ -0,0 +1,9 @@
+#introduction function
+def introduction():
+    print("👋 Hello there!")
+    print("Welcome to my Python learning journey.")
+    print("I'm excited to explore coding, solve problems, and build awesome projects with Python.")
+    print("Let the adventure begin! 🚀🐍")
+
+# Call the function
+introduction()
