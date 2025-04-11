549c64afcc3b8afc7fdb5ccf8a83bbe02e6b2e05
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-11 17:16:21 +0200

    augemented-operators

---
+++ Introduction to Python/operators/augemented-operators.py
@@ -0,0 +1,36 @@
+# augemented arithmetic operations
+
+# addition
+x = 10
+x = x + 7
+print(x)
+
+# subtraction
+x = 10
+x = x - 7
+print(x)
+
+# multiplication
+x = 10
+x = x * 7
+print(x)
+
+# division 1
+x = 10
+x = x / 7
+print(x)
+
+# division 2
+x = 10
+x = x // 7
+print(x)
+
+# modulus
+x = 10
+x = x % 7
+print(x)
+
+# exponent
+x = 10
+x = x ** 7
+print(x)
---
+++ Introduction to Python/operators/operators.py
@@ -0,0 +1,22 @@
+# arithmetic operations
+
+# addition
+print(10+7)
+
+# subtraction
+print(10-7)
+
+# multiplication
+print(10*7)
+
+# division 1
+print(10/7)
+
+# division 2
+print(10//7)
+
+# modulus
+print(10%7)
+
+# exponent
+print(10**7)
