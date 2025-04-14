562bcef94babe72efea1f10dca5dcd1af0f31d15
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-14 16:54:38 +0200

    math-functions

---
+++ Introduction to Python/math functions/math_1.py
@@ -0,0 +1,6 @@
+# rounding off to the nearest ten
+x = 3.9
+print(round(x))
+
+# absolute function
+print(abs(-4))
---
+++ Introduction to Python/math functions/math_2.py
@@ -0,0 +1,6 @@
+# using the math module
+
+import math
+print(math.ceil(3.2))
+
+print(math.floor(3.2))
