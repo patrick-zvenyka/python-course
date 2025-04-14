ab1b93ab1c4c4d4859b404040a021c631a6ca923
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-14 17:26:56 +0200

    logical-op-modified

--- Introduction to Python/operators/logical_op.py
+++ Introduction to Python/operators/logical_op.py
@@ -1,5 +1,16 @@
+# checking eligibility of a client to apply for loan
 has_high_income = True
 has_good_credit = True
+has_criminal_record = False
 
+# AND operator
 if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+# OR operator
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+
+
+if has_high_income and not has_criminal_record:
     print("Eligible for loan")
