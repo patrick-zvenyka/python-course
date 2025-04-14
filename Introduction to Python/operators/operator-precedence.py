0c595b1e7e42e16ce7e4c8539dbcd3b53b337529
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-14 16:37:07 +0200

    operator-precedence

---
+++ Introduction to Python/operators/operator-precedence.py
@@ -0,0 +1,12 @@
+#Operator precedence in Python determines the order in which operations
+# are evaluated in an expression. Here’s a simplified table (from highest
+# to lowest precedence):
+
+#Precedence Level	             Operators
+        #1                     exponentiation
+        #2                 multiplication or division
+        #3                   addition or subtraction
+
+
+x = 4 * 7 - 8 + 5
+print(x)
