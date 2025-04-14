1facecd8faaa67b44e163efe97394e31518da4b4
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-14 12:19:55 +0200

    augmented-operators-update

--- Introduction to Python/operators/augmented-operators.py
+++ Introduction to Python/operators/augmented-operators.py
@@ -2,35 +2,42 @@
 
 # addition
 x = 10
-x = x + 7
+# augmented assignment operator
+x += 7
 print(x)
 
 # subtraction
 x = 10
-x = x - 7
+# augmented assignment operator
+x -= 7
 print(x)
 
 # multiplication
 x = 10
-x = x * 7
+# augmented assignment operator
+x *= 7
 print(x)
 
 # division 1
 x = 10
-x = x / 7
+# augmented assignment operator
+x /= 7
 print(x)
 
 # division 2
 x = 10
-x = x // 7
+# augmented assignment operator
+x //= 7
 print(x)
 
 # modulus
 x = 10
-x = x % 7
+# augmented assignment operator
+x %= 7
 print(x)
 
 # exponent
 x = 10
-x = x ** 7
+# augmented assignment operator
+x **= 7
 print(x)
