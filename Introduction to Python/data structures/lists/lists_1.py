c5580e826450c19d0874b16f0568fd2aee958c47
Patrick Zvenyika <patrickzvenyika1999@gmail.com>
2025-04-18 17:19:33 +0200

    intro-to-lists

---
+++ Introduction to Python/data structures/lists/lists_1.py
@@ -0,0 +1,10 @@
+names = ['John','Mary','Sarah','Lee']
+
+#printing all items in the list
+print(names)
+
+#printing item by index
+print(names[0])
+
+#printing items by index range
+print(names[2:])
