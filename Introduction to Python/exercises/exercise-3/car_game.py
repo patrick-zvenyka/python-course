76f92e6489cb37bd84ce2ffe662f3c61e585cfb0
patrick-zvenyika <patrickzvenyika1999@gmail.com>
2025-04-18 07:16:19 +0200

    car-game-updated

--- Introduction to Python/exercises/exercise-3/car_game.py
+++ Introduction to Python/exercises/exercise-3/car_game.py
@@ -1,22 +1,34 @@
 command = ""
 command_list = [
-    '(command to start car) : start',
-    '(command to stop car) : stop',
-    '(command to quit game) : quit'
+    'start car: start',
+    'stop car: stop',
+    'quit game: quit'
 ]
-
-while command != "quit":
+started = False
+while True:
     command = input("<type command>: ").lower()
 
     if command == 'start':
-        print('Car started.')
+        if started:
+            print('Car already started')
+        else:
+            started = True
+            print('Car started.')
+
     elif command == 'stop':
-        print("Car stopped.")
+        if not started:
+            print('Car is already stopped!')
+        else:
+            started = False
+            print("Car stopped.")
+
+
     elif command == 'help':
         print("Here is the command list:")
         for item in command_list:
             print(f" - {item}")
     elif command == 'quit':
         print("Game exited.")
+        break
     else:
         print("I don't understand that command.")
